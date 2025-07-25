799e81f1f19660e95314d14f72ee355bca632a79
zain-Z <zai1999n@gmail.com>
2025-07-25 22:06:09 +0300

    the intial first commit

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { RoomsModule } from './rooms/rooms.module';
+import { BookingsModule } from './bookings/bookings.module';
+
+@Module({
+  imports: [UsersModule, RoomsModule, BookingsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
